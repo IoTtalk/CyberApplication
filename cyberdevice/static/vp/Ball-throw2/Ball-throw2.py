89fd1f7c7ea932ad80e266c5592afc93a9934a38
Ksoy <ksoyam95@gmail.com>
2022-02-15 20:11:55 +0800

    update Ball-throw2

--- cyberdevice/static/vp/Ball-throw2/Ball-throw2.py
+++ cyberdevice/static/vp/Ball-throw2/Ball-throw2.py
@@ -1,104 +1,106 @@
-
-g=9.8
+scene = None
+g = 9.8
 size = 0.7
-scene = display(width=600, height=500, background=vector(0.6,0.3,0.2), center=vector(-9,7,0))
-floor = box(length=24, height=0.5, width=4, color=color.green, pos=vector(-9,0,0))
-box(length=8, height=0.5, width=4, color=color.red, pos= vector(7,0,0) )
-box(length=8, height=0.5, width=4, color=color.green, pos= vector(15,0,0) )
-
-axis = []
-labels = []
-axisInit()
-
-scene.range=16
-camera_x1=0.8
-camera_x2=0.0
-scene.forward=vector(camera_x1,camera_x2,-1)
+dt = 0.005
+is_gj = 1
 
 preloadAudio('Startup.wav')
 preloadAudio('chord.wav')
 preloadAudio('gj.wav')
 
-def axisInit():
-    global axis, labels
-    a = -21
-    b = 0
-    c = 40
-    d = 10
-    axis.append(arrow(pos=vec(a,b,-2), axis=vec(c+5,0,0), shaftwidth= 0.1, color = color.white))
-    axis.append(arrow(pos=vec(a,b,-2), axis=vec(0,d+5,0), shaftwidth= 0.1, color = color.white))
-    for t in range(0,20):
-        axis.append(box(pos=vec(a + (t+1)*(c/20),b+d/2,-2), length=0.1, height=d, width=0.1))
-    
-    for j in range(0,10):
-        axis.append(box(pos=vec(a + c/2,b + (j+1)*(d/10),-2), length=c, height=0.1, width=0.1,color=color.gray(0.8)))
-    
-    labels.append(label(pos=vec(a + 0*(c/5),b + d+ d/10,-1.5), text = str(0*(c/5)), height = 20, border = 12, font = 'monospace', color = color.white, box = False, opacity=0))
-    for x in range(1,6):
-        num = str(x*(c/5))
-        labels.append(label(pos=vec(a + x*(c/5),b + d + d/10,-2), text = num, height = 20, border = 12, font = 'monospace', color = color.white, box = False, opacity=0))
-    
-    for y in range(0,6):
-        num = str(b+y*(d/5))
-        labels.append(label(pos=vec(a-2*c/40,b + y*(d/5),-2), text = num, height = 20, border = 12, font = 'monospace', color = color.white, box = False))
-def balljump(spd):
-    if (spd < 3): return 
 
-    ball = sphere(pos=vector(-24.5, 10.0, 0.0), radius=size, color=color.white)
+def init():
+    global scene
+    scene = display(width=600,
+                    height=500,
+                    background=vector(0.6, 0.3, 0.2),
+                    center=vector(-12, 7, 0),
+                    range=16,
+                    forward=vector(0.8, 0, -1))
+
+    # floors
+    box(length=24,
+        height=0.5,
+        width=4,
+        color=color.green,
+        pos=vector(-12, 0, 0))
+    box(length=8,
+        height=0.5,
+        width=4,
+        color=color.red,
+        pos=vector(4, 0, 0))
+    box(length=8,
+        height=0.5,
+        width=4,
+        color=color.green,
+        pos=vector(12, 0, 0))
+
+
+def balljump(spd):
+    if (spd < 5):
+        return
+
+    global size
+    ball = sphere(pos=vector(-24, 10.0, 0.0),
+                  radius=size,
+                  color=color.white)
     ball.velocity = vector(spd, -1.0, 0.0)
 
-    dt = 0.003
-    gj_playFlag=1
-
-    def resetScene():
+    def reset_scene():
+        global is_gj
         scene.background = vector(0.6, 0.3, 0.2)
-        
-    
+        is_gj = 1
+
     def jump():
-        global gj_playFlag
+        if (ball.pos.x < 8 and ball.pos.x > 0) and ball.pos.y < size + 0.5:
+            global is_gj
+            scene.background = vector(0, 0, 0)
+            sleep(0.3, reset_scene)
 
-        #console.log(ball.pos.x, ball.pos.y)
-        if ball.pos.x < 15:
-          rate(1000, jump)
-        else:
-          ball.visible = False
-          gj_playFlag = 1 
-          return
+            if is_gj:
+                playAudio('gj.wav')
+                is_gj = 0
 
-        if (ball.pos.x < 10 and ball.pos.x > 1.5) and ball.pos.y < 1 :
-            scene.background = vector(0,0,0)
-            if gj_playFlag: 
-                playAudio('gj.wav') 
-                gj_playFlag=0
-            rate(4, resetScene)
-		
-        previous_x = ball.pos.x
         ball.pos = ball.pos + ball.velocity * dt
 
-        if ball.pos.y < size and ball.velocity.y < 0:
+        if ball.pos.y < size + 0.5:
             ball.velocity.y = - ball.velocity.y
-            playAudio('chord.wav')
+            # playAudio('chord.wav')
         else:
             ball.velocity.y = ball.velocity.y - g * dt
-    
+
+        if ball.pos.x < 16:
+            rate(1 / dt, jump)
+        else:
+            ball.visible = False
+
     jump()
 
+
 def Speed_O(data):
-    if data != None:
+    if data is not None:
         balljump(data[0])
 
+
 def setup(do_id=None):
-    da = iottalkjs.DAI({
+    init()
+
+    def onRegister():
+        playAudio('Startup.wav')
+
+    profile = {
         'apiUrl': ecEndpoint,
         'deviceModel': 'Ball-throw2',
-        'odfList': [Speed_O],
+        'idfList': [],
+        'odfList': [[Speed_O, ['m/s']]],
         'profile': {
             'is_sim': do_id is not None,
             'do_id': do_id,
-        }
-    });
-    da.run();
-    playAudio('Startup.wav')
+        },
+        'onRegister': onRegister,
+    }
+    da = iottalkjs.DAI(profile)
+    da.run()
 
 projectInit({
   'dos': {
